deee16634b6f6fcf3ef8b30f62416c72ee34b8ba
Theo Janke-Furman <tjankefurman@gmail.com>
2022-09-22 15:43:08 -0500

    Header added, cards styled

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,4 +6,10 @@ export default function Header() {
 
     const total = useSelector(store => store.totalPrice);
 
+    return(
+        <header className='App-header'>
+            <h1 className='App-title'>Prime Pizza! 🫠</h1>
+            <h2>Total: {total}</h2>
+        </header>
+    )
 }
