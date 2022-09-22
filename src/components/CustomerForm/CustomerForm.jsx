c1e9441191e61f3c4971c777a1c580164ef740d7
Maggie Whitlock <maggie.whitlock@gmail.com>
2022-09-22 15:38:23 -0500

    complete customer form

--- src/components/CustomerForm/CustomerForm.jsx
+++ src/components/CustomerForm/CustomerForm.jsx
@@ -1,20 +1,21 @@
 // MAGGIE ONLY
 import { useState } from 'react';
 import { useDispatch} from 'react-redux';
+import { useHistory } from 'react-router-dom';
 
 function CustomerForm() {
     const [name, setName] = useState('');
     const [streetAddress, setStreetAddress] = useState('');
     const [city, setCity] = useState('');
     const [zip, setZip] = useState('');
-    const [type, setType] = useState('unchecked');
+    const [type, setType] = useState('');
     
+    const history = useHistory();
+
     const dispatch = useDispatch();
 
     const handleNext = event => {
         event.preventDefault();
-        console.log('in handleNext function');
-        console.log('in handleNext here is what we are going to dispatch:', name, streetAddress, city, zip, type);
         // create action for dispatch with payload object with customer data
         const action = {
             type: 'SET_CUST',
@@ -27,13 +28,14 @@ function CustomerForm() {
             }
         }
         dispatch(action);
+        // NEXT button needs to send us to the Confirm Order page
+        history.push("/ConfirmOrder");
         // clear inputs
         setName('');
         setStreetAddress('');
         setCity('');
         setZip('');
-        setType('unchecked');
-
+        setType('');
     }
 
     return (
@@ -45,37 +47,28 @@ function CustomerForm() {
                     value={name}
                     onChange={(event) => setName(event.target.value)}
                 />
-                
                 <input
                     placeholder="Street Address"
                     value={streetAddress}
                     onChange={(event) => setStreetAddress(event.target.value)}
                 />
-                
                 <input
                     placeholder="City"
                     value={city}
                     onChange={(event) => setCity(event.target.value)}
                 />
-
                 <input
                     placeholder="Zip"
                     value={zip}
                     onChange={(event) => setZip(event.target.value)}
                 />
-
-                <div onChange={(event) => setType(event.target.value)} className="radioButtons">
-                    <input type="radio" value="pickup" name="gender" /> Pickup
-                    <input type="radio" value="delivery" name="gender" /> Delivery
+                <div className="radioButtons"  onChange={(event) => setType(event.target.value)}>
+                    <input type="radio" value="pickup" name="type"/> Pickup
+                    <input type="radio" value="delivery" name="type"/> Delivery
                 </div>
-
                 <button className="nextButton">NEXT</button>
-
             </form>
-        
-
         </>
-    
     )
 }
 export default CustomerForm;
