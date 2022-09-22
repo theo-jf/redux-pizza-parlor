290599bff95631f26c10f7d836a4a5f7cc482681
Theo Janke-Furman <tjankefurman@gmail.com>
2022-09-22 11:45:07 -0500

    Added component files

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,17 +2,25 @@ import React from 'react';
 import axios from 'axios';
 import './App.css';
 
+// Router 
+import { useDispatch, useSelector } from 'react-redux';
+import { HashRouter as Router, Route, Link} from 'react-router-dom';
+
+// Import Components Here
+
 function App() {
 
   return (
     <div className='App'>
-      <header className='App-header'>
-        <h1 className='App-title'>Prime Pizza</h1>
-      </header>
+      <Router>
+        <header className='App-header'>
+          <h1 className='App-title'>Prime Pizza</h1>
+        </header>
+        
+        <img src='images/pizza_photo.png' />
+        <p>Pizza is great.</p>
   
-      <img src='images/pizza_photo.png' />
-      <p>Pizza is great.</p>
-  
+      </Router>
     </div>
   );
 }
