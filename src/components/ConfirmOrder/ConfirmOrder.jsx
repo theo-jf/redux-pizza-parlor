3aece6deb2f4c9427a3867ab7cb2594b82be0907
Theo Janke-Furman <tjankefurman@gmail.com>
2022-09-22 12:01:04 -0500

    Began Confirm Order component

--- src/components/ConfirmOrder/ConfirmOrder.jsx
+++ src/components/ConfirmOrder/ConfirmOrder.jsx
@@ -1,1 +1,19 @@
 // THEO ONLY
+import axios from 'axios';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+
+export default function ConfirmOrder() {
+
+    const dispatch = useDispatch();
+
+    // Load items from line_item table once this page is loaded
+    useEffect(() => {
+        fetchLineItems();
+      }, []);
+
+    const fetchLineItems = () => {
+        axios.get('/')
+    }
+
+}
