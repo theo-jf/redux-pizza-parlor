d6828f781f41fb4b6e07ce8d1c32f38dcc316c36
Sam Freeman <samcfreeman888@gmail.com>
2022-09-22 13:30:04 -0500

    done with pizza list component

--- src/components/PizzaList/PizzaList.jsx
+++ src/components/PizzaList/PizzaList.jsx
@@ -1,11 +1,18 @@
 // SAM ONLY
 import axios from "axios";
 import {useEffect} from 'react';
+import {useDispatch} from 'react-redux';
+import {useSelector} from 'react-redux';
+import PizzaItem from '../PizzaItem/PizzaItem.jsx';
 
 function PizzaList () {
     useEffect(() => {
         fetchPizzaList();
     }, []);
+
+    const pizzas = useSelector(store => store.allPizzas);
+
+    const dispatch = useDispatch();
 
     const fetchPizzaList = () =>{
         axios({
@@ -14,14 +21,27 @@ function PizzaList () {
         })
         .then((response) => {
             console.log(response.data);
+            const action = {
+                type: 'SET_PIZZAS',
+                payload: response.data
+            }
+            dispatch(action);
         })
         .catch((error) => {
             console.log('Error in the get route', error);
         })
     }
+ 
+    
+    //do I need to make the LI for each Item component?
     return(
         <ul>
-            {}
+            {pizzas.map((pizza) => {
+                return(
+                    <PizzaItem
+                    key={pizza.id}/>
+                )
+            })}
         </ul>
     );
 }
