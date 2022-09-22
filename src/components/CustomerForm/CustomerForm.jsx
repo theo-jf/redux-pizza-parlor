1542d1cbfb12581a35b1336d3959eb843deaac49
Maggie Whitlock <maggie.whitlock@gmail.com>
2022-09-22 14:22:02 -0500

    dispatch to redux store

--- src/components/CustomerForm/CustomerForm.jsx
+++ src/components/CustomerForm/CustomerForm.jsx
@@ -1,23 +1,45 @@
 // MAGGIE ONLY
 import { useState } from 'react';
-import { useDispatch} from 'react-redux'
-
-
+import { useDispatch} from 'react-redux';
 
 function CustomerForm() {
     const [name, setName] = useState('');
     const [streetAddress, setStreetAddress] = useState('');
     const [city, setCity] = useState('');
     const [zip, setZip] = useState('');
+    const [type, setType] = useState('unchecked');
+    
+    const dispatch = useDispatch();
 
-    // dispatch SET_CUST
+    const handleNext = event => {
+        event.preventDefault();
+        console.log('in handleNext function');
+        console.log('in handleNext here is what we are going to dispatch:', name, streetAddress, city, zip, type);
+        // create action for dispatch with payload object with customer data
+        const action = {
+            type: 'SET_CUST',
+            payload: {
+                customer_name: name,
+                street_address: streetAddress,
+                city: city,
+                zip: zip,
+                type: type,
+            }
+        }
+        dispatch(action);
+        // clear inputs
+        setName('');
+        setStreetAddress('');
+        setCity('');
+        setZip('');
+        setType('unchecked');
 
-
+    }
 
     return (
         <>
             <h2>Step 2: Customer Information</h2>
-            <form>
+            <form onSubmit={handleNext}>
                 <input
                     placeholder="Name"
                     value={name}
@@ -42,7 +64,7 @@ function CustomerForm() {
                     onChange={(event) => setZip(event.target.value)}
                 />
 
-                <div className="radioButtons">
+                <div onChange={(event) => setType(event.target.value)} className="radioButtons">
                     <input type="radio" value="pickup" name="gender" /> Pickup
                     <input type="radio" value="delivery" name="gender" /> Delivery
                 </div>
