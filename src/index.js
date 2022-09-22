ba44d285d425c66c8336d6eb0edb0cbfd17641d0
Theo Janke-Furman <tjankefurman@gmail.com>
2022-09-22 11:32:48 -0500

    Reducers created

--- src/index.js
+++ src/index.js
@@ -3,4 +3,73 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Import REDUX 
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+
+const selectedPizzas = (state = [], action) => {
+
+    switch(action.type) {
+        case 'ADD_PIZZA':
+            // Adds Pizza Object to state array
+            return [...state, action.payload];
+
+        case 'REM_PIZZA':
+            // Removes Pizza Object from state array
+            return state.filter(pizza => pizza.id != action.payload.id);
+
+        case 'CLEAR_ALL':
+            // State is now an empty array
+            return [];
+    }
+    return state;
+}
+
+const totalPrice = (state = 0, action) => {
+
+    // Adds or removes Pizza Price
+    switch(action.type) {
+        case 'ADD_PIZZA':
+            return state + action.payload.price;
+
+        case 'REM_PIZZA':
+            return state - action.payload.price;
+
+        case 'CLEAR_ALL':
+            return 0;
+    }
+    return state;
+}
+
+const currentCustomer = (state = {}, action) => {
+
+    switch(action.type) {
+        case 'SET_CUSTOMER':
+            return action.payload;
+
+        case 'CLEAR_ALL':
+            return {};
+    }
+    return state;
+}
+
+
+// Create store
+const storeInstance = createStore(
+    combineReducers(
+        {
+            selectedPizzas,
+            totalPrice,
+            currentCustomer
+        }
+    ),
+    applyMiddleware(logger)
+)
+
+
+ReactDOM.render( 
+    <Provider store={storeInstance}>
+        <App />
+    </Provider>, 
+    document.getElementById('root'));
