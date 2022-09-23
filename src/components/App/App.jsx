4e45bf7b95f6158bb15af0dcfb354e2d52e7b03d
Theo Janke-Furman <tjankefurman@gmail.com>
2022-09-23 10:28:53 -0500

    Back button visibility functional

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { useHistory } from "react-router-dom";
 
 // Router 
 import { useDispatch, useSelector } from 'react-redux';
